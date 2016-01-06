03f62e18298bc63c442060d19eb73f696a652b15
Tareq A Khandaker <tareqakhandaker@gmail.com>
2016-01-05 22:10:43 -0500

    Update Stockfighter main class

--- src/main/java/Stockfighter.java
+++ src/main/java/Stockfighter.java
@@ -1,7 +1,10 @@
 import levels.TestEx;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Stockfighter {
+    private static final AtomicBoolean done = new AtomicBoolean(true);
     public static void main(String[] args) {
-        new TestEx().play();
+        new TestEx(done).play();
     }
 }
